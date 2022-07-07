85451b407dc892c95d43f494b0b73327877579d7
Boluwatife2904 <sanvicola2000@gmail.com>
2022-07-07 20:38:12 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="bg-black text-white flex min-h-screen text-2xl items-center justify-center">
+		<h1>Nuxt 3 + Tailwind Starter Files</h1>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
